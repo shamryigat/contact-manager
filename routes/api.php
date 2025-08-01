14059ca529a7e55cf3839f0f1bce9df15ae689e8
Shammm <shamryigat@gmail.com>
2025-08-02 02:10:39 +0800

    Comment out API route definitions for contacts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ContactApiController;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Api\ContactApiController;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('contacts', ContactApiController::class);
-});
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::apiResource('contacts', ContactApiController::class);
+// });
